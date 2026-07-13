17a01c55839b8f09a477bb5440c7bcd140e47abb
Ven <tudurap.com@gmail.com>
2026-07-13 07:46:48 +0100

    fix: NetherliciousCompat and LockTessellator config options being broken

--- src/main/java/com/ventooth/beddium/config/TerrainRenderingConfig.java
+++ src/main/java/com/ventooth/beddium/config/TerrainRenderingConfig.java
@@ -148,13 +148,13 @@ public final class TerrainRenderingConfig {
     @Config.DefaultStringList({"jewelrycraft2:blockCrystal"})
     public static String[] AlwaysTranslucentSprites;
 
-    @Config.Name("LockTessellatorMode")
+    @Config.Name("LockTessellator")
     @Config.Comment({
             "Allows Beddium to 'lock' the tessellator.",
             "Enable to help debug blocks making chunks turn invisible.",
             "Has a minor performance impact on chunk updates.",
     })
-    @Config.LangKey("config.beddium.terrainrendering.LockTessellatorMode")
+    @Config.LangKey("config.beddium.terrainrendering.LockTessellator")
     @Config.RequiresMcRestart
     @Config.DefaultBoolean(false)
     public static boolean LockTessellator;
